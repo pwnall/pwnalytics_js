7af3b2f4e69fd0c96a00ecad00ec71024f4b593c
Victor Costan <costan@gmail.com>
2011-03-28 09:20:45 -0400

    Reduce data transfer by stripping URL query at client side.

--- spec/javascripts/history_spec.js
+++ spec/javascripts/history_spec.js
@@ -1,7 +1,7 @@
 describe("Pwnalytics history ", function() {
   beforeEach(function() {
     Pwnalytics.dropSession();
-    Pwnalytics.initSession();    
+    Pwnalytics.initSession();
   });
   it("should have URL in session", function() {
     expect(Pwnalytics.session.url).toContain("http://");
@@ -9,5 +9,29 @@ describe("Pwnalytics history ", function() {
   });
   it("should have referrer in session", function() {
     expect(Pwnalytics.session.ref).not.toBeNull();
+  });
+  
+  describe("normalizeUrl", function() {
+    it("should leave query-less URLs alone", function() {
+      expect(Pwnalytics.normalizeUrl("http://localhost:3000")).
+          toBe("http://localhost:3000");
+    });
+    it("should strip query off URLs", function() {
+      expect(Pwnalytics.normalizeUrl("http://localhost:3000?q=awesome")).
+          toBe("http://localhost:3000?");
+    });
+  });
+  
+  describe("session URLs", function() {
+    beforeEach(function() {
+      spyOn(Pwnalytics, "normalizeUrl").andReturn("normalized");
+      Pwnalytics.dropSession();
+      Pwnalytics.initSession();
+    });
+    
+    it("should normalize location", function() {
+      expect(Pwnalytics.session.url).toBe("normalized");
+      expect(Pwnalytics.session.ref).toBe("normalized");
+    });
   });
 });
